476a857f39ca866434b7baee983ecbe9316d6bb0
drussell1974 <dave@jazzthecat.co.uk>
2020-06-16 06:49:28 +0100

    fix incorrect variable name

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -3,6 +3,7 @@
 # Give permission to subnet - e.g. /mnt/nfs_share 192.168.1.0/24(rw,sync,no_subtree_check)
 # requires NFS_IPADDR_ALLOWED environment variable
 
+
 echo "docker-entrypoint.sh: Allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/${NFS_PUBLIC_PATH} in /etc/export/ ..."
 
 echo "/mnt/nfs_share/${NFS_PUBLIC_PATH} ${NFS_IPADDR_ALLOWED}(rw,sync,no_subtree_check)" > /etc/exports
@@ -12,20 +13,15 @@ echo "docker-entrypoint.sh: Allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/${N
 
 echo "/mnt/nfs_share/${NFS_HOME_PATH} ${NFS_IPADDR_ALLOWED}(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 
+
 # refresh nfs service
 
 echo "docker-entrypoint.sh: Applying exportfs and restarting nfs-kernel-server..."
-
 exportfs -a && service nfs-kernel-server reload
 
 echo "docker-entrypoint.sh: Getting Contents of root share directory /mnt/nfs_share/ ..."
-ls -l /mnt/nfs_share
+ls -l /mnt/nfs_share/
 
-echo "docker-entrypoint.sh: Getting contents of subdirectory /mnt/nfs_share/${NFS_HOME_PATH} ..."
-ls -l /mnt/nfs_share/${NFS_HOME_PATH}
-
-echo "docker-entrypoint.sh: Getting contents of subdirectory /mnt/nfs_share/${NFS_PULIC_PATH} ..."
-ls -l /mnt/nfs_share/${NFS_PUBLIC_PATH}
 
 # prevent docker from exiting
 
