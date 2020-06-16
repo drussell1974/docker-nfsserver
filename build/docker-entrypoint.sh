82ef39855876833d51504e1a42fbe9819c1513d5
drussell1974 <dave@jazzthecat.co.uk>
2020-06-16 06:31:55 +0100

    add extra logging in script

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -3,24 +3,33 @@
 # Give permission to subnet - e.g. /mnt/nfs_share 192.168.1.0/24(rw,sync,no_subtree_check)
 # requires NFS_IPADDR_ALLOWED environment variable
 
-echo "Allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/${NFS_PUBLIC_PATH} in /etc/export/ ..."
+echo "docker-entrypoint.sh: Allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/${NFS_PUBLIC_PATH} in /etc/export/ ..."
 
 echo "/mnt/nfs_share/${NFS_PUBLIC_PATH} ${NFS_IPADDR_ALLOWED}(rw,sync,no_subtree_check)" > /etc/exports
 
 
-echo "Allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/${NFS_HOME_PATH} in /etc/export/ ..."
+echo "docker-entrypoint.sh: Allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/${NFS_HOME_PATH} in /etc/export/ ..."
 
 echo "/mnt/nfs_share/${NFS_HOME_PATH} ${NFS_IPADDR_ALLOWED}(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 
 # refresh nfs service
 
-echo "Applying exportfs and restarting nfs-kernel-server..."
+echo "docker-entrypoint.sh: Applying exportfs and restarting nfs-kernel-server..."
 
 exportfs -a && service nfs-kernel-server reload
 
+echo "docker-entrypoint.sh: Getting Contents of root share directory /mnt/nfs_share/ ..."
+ls -l /mnt/nfs_share
+
+echo "docker-entrypoint.sh: Getting contents of subdirectory /mnt/nfs_share/${NFS_HOME_PATH} ..."
+ls -l /mnt/nfs_share/${NFS_HOME_PATH}
+
+echo "docker-entrypoint.sh: Getting contents of subdirectory /mnt/nfs_share/${NFS_PULIC_PATH} ..."
+ls -l /mnt/nfs_share/${NFS_PUBLIC_PATH}
+
 # prevent docker from exiting
 
-echo "keep alive"
+echo "docker-entrypoint.sh: keep alive!"
 tail -f /dev/null
 
 
