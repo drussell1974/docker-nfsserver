2d798146e3ad551aa9a7d55d7d147feceef0f7e2
drussell1974 <dave@jazzthecat.co.uk>
2020-06-16 10:10:27 +0100

    add portmap and create volumes during compose

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
 
+echo "change options in port maps and allow sub net in hosts for port maps"
+perl -pi -e "s/^OPTIONS/#OPTIONS/" /etc/default/portmap
+echo "portmap: 192.168.1.0" >> /etc/hosts.allow
+
 # Give permission to subnet - e.g. /mnt/nfs_share 192.168.1.0/24(rw,sync,no_subtree_check)
 # requires NFS_IPADDR_ALLOWED environment variable
 
 
-echo "docker-entrypoint.sh: Allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/${NFS_PUBLIC_PATH} in /etc/export/ ..."
+echo "docker-entrypoint.sh: Allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/${NFS_PUBLIC_PATH} in /etc/exports ..."
 
 echo "/mnt/nfs_share/${NFS_PUBLIC_PATH} ${NFS_IPADDR_ALLOWED}(rw,sync,no_subtree_check)" > /etc/exports
 
 
-echo "docker-entrypoint.sh: Allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/${NFS_HOME_PATH} in /etc/export/ ..."
+echo "docker-entrypoint.sh: Allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/${NFS_HOME_PATH} in /etc/exports ..."
 
 echo "/mnt/nfs_share/${NFS_HOME_PATH} ${NFS_IPADDR_ALLOWED}(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 
 
 # refresh nfs service
 
-echo "docker-entrypoint.sh: Applying exportfs and restarting nfs-kernel-server..."
-exportfs -a && service nfs-kernel-server reload
+echo "docker-entrypoint.sh: Applying exportfs and restarting portmap and nfs-kernel-server..."
+exportfs -a && \
+service nfs-kernel-server reload && \
+service rpcbind restart
 
 echo "docker-entrypoint.sh: Getting Contents of root share directory /mnt/nfs_share/ ..."
 ls -l /mnt/nfs_share/
