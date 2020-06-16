4d93a432c5508682824b4f63d0d88a2e81559f50
drussell1974 <dave@jazzthecat.co.uk>
2020-06-16 16:17:15 +0100

    use environment variables

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
-
 echo "docker-entrypoint.sh: change options in port maps and allow sub net in hosts for port maps"
 perl -pi -e "s/^OPTIONS/#OPTIONS/" /etc/default/portmap
-echo "portmap: 192.168.1.0" >> /etc/hosts.allow
+echo "portmap: ${NFS_IPADDR_ALLOWED}" >> /etc/hosts.allow
 
-# Give permission to subnet - e.g. /mnt/nfs_share 192.168.1.0/24(rw,sync,no_subtree_check)
+# Give permission to subnet - e.g. /mnt/nfs_share/ 192.168.1.0/24(rw,sync,no_subtree_check)
 # requires NFS_IPADDR_ALLOWED environment variable
 
 
-echo "docker-entrypoint.sh: allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/public in /etc/exports ..."
+echo "docker-entrypoint.sh: allowing ${NFS_IPADDR_ALLOWED} to ${NFS_PUBLIC_CONT} in /etc/exports ..."
 
-echo "/usr/nfs_server/nfs_share/public ${NFS_IPADDR_ALLOWED}(rw,sync,no_subtree_check)" > /etc/exports
+echo "${NFS_PUBLIC_CONT} ${NFS_IPADDR_ALLOWED}(rw,sync,no_subtree_check)" > /etc/exports
 
 
-echo "docker-entrypoint.sh: allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/home in /etc/exports ..."
+echo "docker-entrypoint.sh: allowing ${NFS_IPADDR_ALLOWED} to ${NFS_HOME_CONT} in /etc/exports ..."
 
-echo "/usr/nfs_server/nfs_share/home ${NFS_IPADDR_ALLOWED}(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "${NFS_HOME_CONT} ${NFS_IPADDR_ALLOWED}(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 
 
 # refresh nfs service
@@ -25,11 +24,11 @@ exportfs -a && \
 service nfs-kernel-server reload && \
 service rpcbind restart
 
-echo "docker-entrypoint.sh: showing contents of /usr/nfs_server/nfs_share/hone ..."
-ls -l /usr/nfs_server/nfs_share/home
+echo "docker-entrypoint.sh: showing contents of ${NFS_HOME_CONT} ..."
+ls -l ${NFS_HOME_CONT}
 
-echo "docker-entrypoint.sh: showing contents of /usr/nfs_server/nfs_share/public ..."
-ls -l /usr/nfs_server/nfs_share/public
+echo "docker-entrypoint.sh: showing contents of ${NFS_PUBLIC_CONT} ..."
+ls -l ${NFS_PUBLIC_CONT}
 
 # prevent docker from exiting
 
