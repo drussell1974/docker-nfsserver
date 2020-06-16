41fabc091514cedf6e42271b008bcc8913336bf3
drussell1974 <dave@jazzthecat.co.uk>
2020-06-16 14:28:03 +0100

    change location of nfs_share to working directory #5

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "change options in port maps and allow sub net in hosts for port maps"
+echo "docker-entrypoint.sh: change options in port maps and allow sub net in hosts for port maps"
 perl -pi -e "s/^OPTIONS/#OPTIONS/" /etc/default/portmap
 echo "portmap: 192.168.1.0" >> /etc/hosts.allow
 
@@ -8,26 +8,28 @@ echo "portmap: 192.168.1.0" >> /etc/hosts.allow
 # requires NFS_IPADDR_ALLOWED environment variable
 
 
-echo "docker-entrypoint.sh: Allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/public in /etc/exports ..."
+echo "docker-entrypoint.sh: allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/public in /etc/exports ..."
 
-echo "/mnt/nfs_share/public ${NFS_IPADDR_ALLOWED}(rw,sync,no_subtree_check)" > /etc/exports
+echo "/usr/nfs_server/nfs_share/public ${NFS_IPADDR_ALLOWED}(rw,sync,no_subtree_check)" > /etc/exports
 
 
-echo "docker-entrypoint.sh: Allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/home in /etc/exports ..."
+echo "docker-entrypoint.sh: allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/home in /etc/exports ..."
 
-echo "/mnt/nfs_share/home ${NFS_IPADDR_ALLOWED}(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "/usr/nfs_server/nfs_share/home ${NFS_IPADDR_ALLOWED}(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 
 
 # refresh nfs service
 
-echo "docker-entrypoint.sh: Applying exportfs and restarting portmap and nfs-kernel-server..."
+echo "docker-entrypoint.sh: applying exportfs and restarting portmap and nfs-kernel-server..."
 exportfs -a && \
 service nfs-kernel-server reload && \
 service rpcbind restart
 
-echo "docker-entrypoint.sh: Getting Contents of root share directory /mnt/nfs_share/ ..."
-ls -l /mnt/nfs_share/
+echo "docker-entrypoint.sh: showing contents of /usr/nfs_server/nfs_share/hone ..."
+ls -l /usr/nfs_server/nfs_share/home
 
+echo "docker-entrypoint.sh: showing contents of /usr/nfs_server/nfs_share/public ..."
+ls -l /usr/nfs_server/nfs_share/public
 
 # prevent docker from exiting
 
