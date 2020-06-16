46e5ae1cf17d6da8788d23521de5c9c695e7cdba
drussell1974 <dave@jazzthecat.co.uk>
2020-06-16 05:35:37 +0100

    add both public share and home directory access

--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -3,10 +3,18 @@
 # Give permission to subnet - e.g. /mnt/nfs_share 192.168.1.0/24(rw,sync,no_subtree_check)
 # requires NFS_IPADDR_ALLOWED environment variable
 
-echo "Create export for...${NFS_IPADDR_ALLOWED}"
-echo "/mnt/nfs_share ${NFS_IPADDR_ALLOWED}(rw,sync,no_subtree_check)" > /etc/exports
+echo "Allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/${NFS_PUBLIC_PATH} in /etc/export/ ..."
+
+echo "/mnt/nfs_share/${NFS_PUBLIC_PATH} ${NFS_IPADDR_ALLOWED}(rw,sync,no_subtree_check)" > /etc/exports
+
+
+echo "Allowing ${NFS_IPADDR_ALLOWED} to /mnt/nfs_share/${NFS_HOME_PATH} in /etc/export/ ..."
+
+echo "/mnt/nfs_share/${NFS_HOME_PATH} ${NFS_IPADDR_ALLOWED}(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 
 # refresh nfs service
+
+echo "Applying exportfs and restarting nfs-kernel-server..."
 
 exportfs -a && service nfs-kernel-server reload
 
